dfdd4ffb91a74cf50df48413b9ee610196b43ef0
daqing7 <2295957879@qq.com>
2020-03-09 00:23:27 +0800

    3-9-1

--- web/src/pages/Classify/component/Table/index.js
+++ web/src/pages/Classify/component/Table/index.js
@@ -1,16 +1,19 @@
 /* eslint-disable no-nested-ternary */
 import React from 'react';
-import { Table, Spin, Empty } from 'antd';
+import { Table, Spin, Empty, Tag, message, Form } from 'antd';
 import htmlToDraft from 'html-to-draftjs';
 import { EditorState, ContentState } from 'draft-js';
 import { Editor } from 'react-draft-wysiwyg';
+import FormElement from '@/components/FormElement';
 import Modal from '@/common/components/Modal';
 import {
   downloadAnnex
 } from '@/services/annexService';
 import styles from './style.less';
 
-export default class Tables extends React.Component {
+const { CheckableTag } = Tag;
+
+class Tables extends React.Component {
 
   columns = [{
     title: '文章名',
@@ -42,6 +45,22 @@ export default class Tables extends React.Component {
       <span>{record.articleForm === 0 ? '在线发布' : '附件上传'}</span>
     )
   }, {
+    title: '操作',
+    dataIndex: 'conduct',
+    render: (_, record, index) => {
+      const { articlename } = record;
+      const that = this;
+      const { choose1Index, choose2Index, choose3Index, choose4Index } = that.state;
+      return (
+        <>
+          <CheckableTag color="#2db7f5" checked={choose1Index.includes(index)} onChange={() => that.handleArticle(articlename, 1, index)}>点赞</CheckableTag>
+          <CheckableTag color="#000" checked={choose2Index.includes(index)} onChange={() => that.handleArticle(articlename, 2, index)}>拉黑</CheckableTag>
+          <CheckableTag color="#87d068" checked={choose3Index.includes(index)} onChange={() => that.handleArticle(articlename, 3, index)}>收藏</CheckableTag>
+          <CheckableTag color="#f50" checked={choose4Index.includes(index)} onChange={() => that.handleArticle(articlename, 4, index)}>评论</CheckableTag>
+        </>        
+      )
+    }
+  }, {
     title: "文章内容",
     datIndex: 'articleContent',
     render: (_, record) => {
@@ -54,7 +73,14 @@ export default class Tables extends React.Component {
 
   state = {
     visible: false,
-    editorState: ''
+    commentModalVisible: false,
+    editorState: '',
+    choose1Index: [],
+    choose2Index: [],
+    choose3Index: [],
+    choose4Index: [],
+    commentArticlename: '',
+    commentIndex: -1,
   }
 
   handleClick = (name, articleForm, articleContent) => {
@@ -71,6 +97,66 @@ export default class Tables extends React.Component {
     }
   }
 
+  handleArticle = (articlename, key, index) => {
+    const { choose1Index, choose2Index, choose3Index, choose4Index } = this.state;
+    if (key === 1) {
+      if (choose1Index.includes(index)) {
+        choose1Index.splice(choose1Index.indexOf(index), 1);
+        message.success('取消点赞成功');
+      } else {
+        message.success('点赞成功');
+        choose1Index.push(index);
+        choose2Index.splice(choose2Index.indexOf(index), 1);
+      }
+      this.setState({
+        choose1Index,
+        choose2Index
+      })
+    } else if (key === 2) {
+      if (choose2Index.includes(index)) {
+        choose2Index.splice(choose2Index.indexOf(index), 1);
+        message.success('取消拉黑成功');
+      } else {
+        message.success('拉黑成功');
+        choose2Index.push(index);
+        choose1Index.splice(choose1Index.indexOf(index), 1);
+      }
+      this.setState({
+        choose2Index,
+        choose1Index
+      })
+    } else if (key === 3) {
+      if (choose3Index.includes(index)) {
+        choose3Index.splice(choose3Index.indexOf(index), 1);
+        message.success('取消收藏成功');
+      } else {
+        message.success('收藏成功');
+        choose3Index.push(index);
+      }
+      this.setState({
+        choose3Index
+      })
+    } else if (key === 4) {
+      if (choose4Index.includes(index)) {
+        choose4Index.splice(choose4Index.indexOf(index), 1);
+        message.success('取消评论成功')
+        this.setState({
+          commentModalVisible: false
+        })
+      } else {
+        choose4Index.push(index);
+        this.setState({
+          commentModalVisible: true,
+          commentIndex: index,
+          commentArticlename: articlename
+        })
+      }
+      this.setState({
+        choose4Index
+      })
+    }
+  }
+
   handleOk = () => {
     this.setState({
       visible: false
@@ -83,9 +169,35 @@ export default class Tables extends React.Component {
     })
   }
 
+  handleCommentOk = () => {
+    this.setState({
+      commentModalVisible: false
+    })
+    message.success('评论成功')
+  }
+
+  handleCommentCancel = () => {
+    const { commentIndex, choose4Index } = this.state;
+    choose4Index.splice(choose4Index.indexOf(commentIndex), 1);
+    this.setState({
+      commentModalVisible: false,
+      choose4Index
+    })
+    message.success('评论取消失败')
+  }
+
   render() {
-    const { loading, dataSource } = this.props;
-    const { editorState, visible } = this.state;
+    const { loading, dataSource, form } = this.props;
+    const { editorState, visible, commentModalVisible, commentIndex, commentArticlename } = this.state;
+    const dataSources = [{
+      articlename: '水浒'
+    }, {
+      articlename: '三国'
+    }]
+    const formElementProps = {
+      form,
+      width: 300
+    }
     return (
       <>
         {
@@ -94,7 +206,7 @@ export default class Tables extends React.Component {
           ) :
             dataSource.length > 0 ? (
               <Table
-                dataSource={dataSource}
+                dataSource={dataSources}
                 loading={loading}
                 columns={this.columns}
               />
@@ -112,7 +224,30 @@ export default class Tables extends React.Component {
             editorState={editorState}
           />
         </Modal>
+        <Modal
+          visible={commentModalVisible}
+          title={`评论文章-${commentArticlename}`}
+          onOk={this.handleCommentOk}
+          onCancel={this.handleCommentCancel}
+          showOk={true}
+          showCancel={true}
+          okText="确认"
+          cancelText="取消"
+        >
+          <Form style={{ padding: 10 }}>
+            <FormElement 
+              {...formElementProps}
+              field="comment"
+              label=""
+              type="textarea"
+              placeholder="请输入评论内容..."
+              rows={5}
+            />
+          </Form>
+        </Modal>
       </>
     )
   }
 }
+
+export default Form.create({ name: 'Tables'})(Tables);
