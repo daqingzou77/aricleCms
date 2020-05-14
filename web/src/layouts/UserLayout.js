fd76e7b940a65e90190d3ce17e7b0daa90a1cf18
daqing7 <2295957879@qq.com>
2020-05-14 10:44:54 +0800

    5-14修改樣式

--- web/src/layouts/UserLayout.js
+++ web/src/layouts/UserLayout.js
@@ -1,17 +1,11 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import { connect } from 'dva';
 import Link from 'umi/link';
 import DocumentTitle from 'react-document-title';
-import { Icon } from 'antd';
+import getPageTitle from '@/utils/getPageTitle';
 import styles from './UserLayout.less';
 import logo from '../assets/logo.ico';
-import getPageTitle from '@/utils/getPageTitle';
 
-const copyright = (
-  <Fragment>
-    Copyright <Icon type="copyright" />  2020 桂林电子科技大学
-  </Fragment>
-);
 
 class UserLayout extends Component {
   componentDidMount() {
@@ -42,7 +36,7 @@ class UserLayout extends Component {
                   <span className={styles.title}>文章信息管理系统</span>
                 </Link>
               </div>
-              <div className={styles.desc}>支持隐私保护的多关键词搜索技术</div>
+              <div className={styles.desc}>支持多关键词搜索技术</div>
             </div>
             {children}
           </div>
